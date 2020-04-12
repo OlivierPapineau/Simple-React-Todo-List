cd77f617205361a236f49388795aaafa7d4a262f
Olivier Papineau <olivier.12.papineau@gmail.com>
2020-04-12 15:18:01 -0400

    [FEATURE] added navigation highlighting

--- src/components/_parts/Nav.tsx
+++ src/components/_parts/Nav.tsx
@@ -1,7 +1,6 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, MouseEvent, useContext } from 'react';
 import { TodoContext } from '../../contexts';
-import { Link } from 'react-router-dom';
-import { AllTodos, CompletedTodos } from '../../pages';
+import { Link, NavLink } from 'react-router-dom';
 import { ITodoItem } from '../../contexts/typings';
 
 const Nav = () => {
@@ -14,10 +13,14 @@ const Nav = () => {
 			<nav>
 				<ul>
 					<li>
-						<Link to="/">All todos</Link>
+						<NavLink exact={true} to="/" activeClassName="link-is-active">
+							All todos
+						</NavLink>
 					</li>
 					<li>
-						<Link to="/completed">Completed todos</Link>
+						<NavLink to="/completed" activeClassName="link-is-active">
+							Completed todos
+						</NavLink>
 						{completedTodos.length > 0 && <span className="number-completed">{completedTodos.length}</span>}
 					</li>
 				</ul>
